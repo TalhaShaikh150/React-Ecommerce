ffa94b100bda58f6df7ec69ed7483cdba85784f0
TalhaShaikh150 <tssmart233@gmail.com>
2025-11-27 02:03:12 +0500

    Initialized Backend

---
+++ server/src/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const PORT = 3000;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello From Home Route");
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running");
+});
